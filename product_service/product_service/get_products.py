ad4daa8eff91db95196444b297e87b9f7e6852cc
Vitaliy Ukrainskiy <vitaliy.ukrainskiy@gameloft.com>
2024-06-15 19:59:20 +0300

    add product service with unit tests

---
+++ product_service/product_service/get_products.py
@@ -0,0 +1,15 @@
+from aws_cdk import aws_lambda as _lambda
+from aws_cdk import Stack
+from constructs import Construct
+
+
+class GetProducts(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        self.get_product_list = _lambda.Function(self, 'GetProductsListHandler',
+                                                 runtime=_lambda.Runtime.PYTHON_3_11,
+                                                 code=_lambda.Code.from_asset('product_service/lambda_func/'),
+                                                 handler='product_list.lambda_handler'
+                                                 )
---
+++ product_service/product_service/lambda_func/product_list.py
@@ -0,0 +1,48 @@
+import json
+
+
+def lambda_handler(event, context):
+    products = [
+        {
+            "id": "1",
+            "name": "Golden Retriever",
+            "description": "Golden Retrievers are friendly, intelligent, and devoted. They are great family pets.",
+            "price": 120
+        },
+        {
+            "id": "2",
+            "name": "German Shepherd",
+            "description": "German Shepherds are confident, courageous, and smart. They are excellent working dogs.",
+            "price": 1200
+        },
+        {
+            "id": "3",
+            "name": "Bulldog",
+            "description": "Bulldogs are calm, courageous, and friendly. They are known for their loose, wrinkled skin.",
+            "price": 1500
+        },
+        {
+            "id": "4",
+            "name": "Beagle",
+            "price": 800,
+            "description": "Beagles are curious, friendly, and merry. They are known for their excellent sense of smell."
+        },
+        {
+            "id": "5",
+            "name": "Poodle",
+            "price": 900,
+            "description": "Poodles are active, proud, and very smart. They come in three sizes: standard, miniature, and toy."
+        }
+    ]
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(products),
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET"
+        }
+    }
+
+    return response
